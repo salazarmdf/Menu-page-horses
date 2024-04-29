08a12c9f802bba36735e027d59f8fba44a676cbe
deivy Lujan <deivylujans@gmail.com>
2024-04-29 12:47:40 -0500

    horses

---
+++ JS/script.js
@@ -0,0 +1,25 @@
+// let click = document.querySelector('#horsejob1');
+
+// click = addEventListener('click', e => {
+//     e.style.width = '340px';
+//     e.style.background = 'none';
+// });
+//no funciona?
+
+// function show(param_div_id){
+//     var x = document.getElementById("div1")
+//     var y = document.getElementById("div2");
+//     if(param_div_id === "div1"){
+//         x.style.display = "block";
+//         y.style.display ="none";
+
+//         document.getElementById("menu1").className = "active";
+//         document.getElementById("menu2").className = "desactive";
+//     }else if(param_div_id === "div2"){
+//         x.style.display = "none";
+//         y.style.display = "block";
+
+//         document.getElementById("menu1").className = "desactive";
+//         document.getElementById("menu2").className = "active";
+//     }
+// }
